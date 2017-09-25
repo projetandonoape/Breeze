5c47300ad60d430b3033e69bd970906c90d0291a
Projetando no Apê <31907535+projetandonoape@users.noreply.github.com>
2017-09-25 14:57:07 -0300

    Update Serial.ino

--- examples/Serial/Serial.ino
+++ examples/Serial/Serial.ino
@@ -1,29 +1,18 @@
 
-#include <Wire.h>
-#include "Breeze.h"
+#include <Wire.h>       //Biblioteca do Arduino para comunicação I2C
+#include "Breeze.h"     //Biblioteca para controlar o Breeze
 
 
 void setup() {
-  Serial.begin(57600);
-  Wire.begin();         //Inicia a Interface I2C 
-  Breeze.reset();      //RESETA VOLUME E BANDAS PARA ZERO
-  //delay(250);
- 
-Breeze.equalize(1,7);  
-Breeze.equalize(2,7);
-
-Breeze.equalize(5,7);
-Breeze.equalize(6,7);
-
-Breeze.equalize(9,7);
-Breeze.equalize(10,7);
-
+  Wire.begin();         //Inicia a Interface I2C
+  Breeze.reset();       //Zera volume e níveis (Breeze não zera níveis e volume quando energizado)
 }
 
 
 void loop() {
- //Breeze.equalize(2,7);
-   delay(1000);
-//for (int i = 1 ; i <= 4; i++) Breeze.equalize (i, 7);   //TODOS OS CANAIS NO NÌVEL +7 (MÁXIMO)
-  delay(25000);
+  
+Breeze.equalize(1,7);   //Exemplo de reforço do canal 1 (32Hz) para nível 7 (+6dB)
+Breeze.equalize(6,-7);  //Exemplo de atenuação do canal 1 (32Hz) para nível -7 (-6dB)
+Breeze.volume (-2);    //Exemplo de atenuação do volume para nível -2
+
 }
