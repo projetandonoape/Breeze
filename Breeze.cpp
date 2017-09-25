93fb5e25e0daf6d17ac60757a68de3ed37b9c262
Projetando no Apê <31907535+projetandonoape@users.noreply.github.com>
2017-09-25 09:29:38 -0300

    Update Breeze.cpp

--- Breeze.cpp
+++ Breeze.cpp
@@ -22,65 +22,65 @@ Observação: os números foram tradatos como binários para a observação da p
 
 */
 
-#include "Wire.h"						//Chama a biblioteca de comunicação I2C 
+#include "Wire.h"						//chama a biblioteca de comunicação I2C 
 #include "Breeze.h"						
 
 Breeze::Breeze() 						//função 'Construtor de Objeto' não utilizado pelo Breeze
 {
 }
 
-void Breeze::equalize (int canal, int nivel) {			//Função de equalização. Recebe o n° e nível do canal a ser ajustado
+void Breeze::equalize (int canal, int nivel) {			//função de equalização. recebe o n° e nível do canal a ser ajustado
 
 	byte parte_A = B0000000;				//1°byte: ENDEREÇO (preenche máscara de bits com zeros)
 	byte parte_B = B10000000;				//2°byte: VOLUME (nesta função não altera o volume)
 	int parte_C = B00000000;				//3°byte: CANAL + NÍVEL (preenche máscara bits com zeros)
-	int posicao[11] = {0, 					//A posição dos canais é dada pelo fabricante do TDA7317 para manter a distorção harmônica (THD) baixa
+	int posicao[11] = {0, 					//a posição dos canais é dada pelo fabricante do TDA7317 para manter a distorção harmônica (THD) baixa
 		B11000000, B11000000, 				/*BANDAS 1 e 2: ENDEREÇO n°B100 corresponde ao CANAL n°5 */
 		B10010000, B10010000, 				/*BANDAS 3 e 4: ENDEREÇO n°B001 corresponde ao CANAL n°2 */
 		B10100000, B10100000, 				/*BANDAS 5 e 6: ENDEREÇO n°B010 corresponde ao CANAL n°3 */
 		B10110000, B10110000,				/*BANDAS 7 e 8: ENDEREÇO n°B011 corresponde ao CANAL n°4 */
 		B10000000, B10000000};				/*BANDAS 9 e 10: ENDEREÇO n°B000 corresponde ao CANAL n°1 */
-	const byte CI1 = B1000010;				//Endereço do CI-1 0x84(hexa)
-	const byte CI2 = B1000011;				//Endereço do CI-2 0x86(hexa)
+	const byte CI1 = B1000010;				//endereço do CI-1 0x84(hexa)
+	const byte CI2 = B1000011;				//endereço do CI-2 0x86(hexa)
 
-	if (canal % 2 == 0) parte_A = CI1; 			//Canais pares são tratadas pelo CI-1;
-	else parte_A = CI2;					//Canais ímpares são tratadas pelo CI-2;
+	if (canal % 2 == 0) parte_A = CI1; 			//canais pares são tratadas pelo CI-1;
+	else parte_A = CI2;					//canais ímpares são tratadas pelo CI-2;
 
-	if (nivel < 0) {					//Valor inferior a 0 indica atenuação
-		parte_C = parte_C | B10001000;			//Concatena parte_C com byte 1 na posição 4 indicando atenuação usando função 'OU' (|)
-		nivel = abs(nivel);				//Módulo do valor, para não enviar valor negativo
+	if (nivel < 0) {					//valor inferior a 0 indica atenuação
+		parte_C = parte_C | B10001000;			//concatena parte_C com byte 1 na posição 4 indicando atenuação usando função 'OU' (|)
+		nivel = abs(nivel);				//módulo do valor, para não enviar valor negativo
 	}
 
-	parte_C = parte_C | posicao [canal] | nivel;		//Concatena os dados a serem enviados no byte C com a função 'OU' (|)
+	parte_C = parte_C | posicao [canal] | nivel;		//concatena os dados a serem enviados no byte C com a função 'OU' (|)
 
-	TDA7317 (parte_A, parte_B, parte_C);			//Chama a função que envia os dados
+	TDA7317 (parte_A, parte_B, parte_C);			//chama a função que envia os bytes
 
 }
 
-void Breeze::reset() {						//Função reset
+void Breeze::reset() {						//função reset
 
-for (byte canal = 1; canal <= 10; canal++) equalize (canal, 0);	//Zera todos os canais
-TDA7317 (B1000010, B00000000, B00000000);			//Zera volume do CI-1
-TDA7317 (B1000011, B00000000, B00000000);			//Zera volume do CI-2
+for (byte canal = 1; canal <= 10; canal++) equalize (canal, 0);	//zera todos os canais
+TDA7317 (B1000010, B00000000, B00000000);			//zera volume do CI-1
+TDA7317 (B1000011, B00000000, B00000000);			//zera volume do CI-2
 }
 
 
-void Breeze::volume (int nivel) {				//Função volume. Recebe o nível do volume
+void Breeze::volume (int nivel) {				//função volume. recebe o nível do volume
 
-	nivel = abs(nivel);					//Módulo do valor, para não enviar valor negativo
-	byte parte_A = B1000010;				//Para manter qualidade de áudio apenas o CI-1 controla o volume
+	nivel = abs(nivel);					//módulo do valor, para não enviar valor negativo
+	byte parte_A = B1000010;				//para manter qualidade de áudio apenas o CI-1 controla o volume
 	int parte_B = B00000000 | nivel;			//2ºbyte: VOLUME (0 OR nivel) 
 	byte parte_C = B00000000; 				//3°byte: CANAL + NIVEL (Não altera nível nesta função)
-	TDA7317 (parte_A, parte_B, parte_C);			//Chama a função que envia os dados
+	TDA7317 (parte_A, parte_B, parte_C);			//chama a função que envia os bytes
 
 }
 
 
-void Breeze::TDA7317 (byte parte_A, byte parte_B, byte parte_C) {//Função avançada: envia as 3 partes do byte (A, B, C) ao Breeze
+void Breeze::TDA7317 (byte parte_A, byte parte_B, byte parte_C) {//função avançada: envia as 3 partes do byte A, B, C ao Breeze
 
 Wire.beginTransmission(parte_A);				//inicia enviando o endereço do CircuitoIntegrado que irá atuar
-Wire.write(parte_B);						//Envia nível de volume
-Wire.write(parte_C);						//Envia canal e nível de atuação
-Wire.endTransmission();						//Finaliza a transmissão
+Wire.write(parte_B);						//envia nível de volume
+Wire.write(parte_C);						//envia canal e nível de atuação
+Wire.endTransmission();						//finaliza a transmissão
 
 }
