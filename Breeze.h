05f81151c7853f3a714bd5d99dbda5d0e3bc7734
Projetando no Apê <31907535+projetandonoape@users.noreply.github.com>
2018-03-09 18:32:23 -0300

    Update Breeze.h

--- Breeze.h
+++ Breeze.h
@@ -17,7 +17,7 @@ class Breeze
 
 public:
 	Breeze ();							//função construtor não utilizada
-	void equalize  (int canal, int nivel) ;				//função para atuar nos níveis dos canais
+	void equalize  (int banda, int nivel) ;				//função para atuar nos níveis das bandas
 	void reset ();							//função zerar todos os canais e volume
 	void volume (int nivel);					//função de atuar apenas no volume
     	void TDA7317 (byte parte_A, byte parte_B, byte parte_C);	//função avançada para enviar bytes ao CircuitoIntegrado
