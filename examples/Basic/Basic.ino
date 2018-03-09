400e702dddad8a91f0ef0ff89045faa1574d065c
Projetando no Apê <31907535+projetandonoape@users.noreply.github.com>
2018-03-09 18:27:45 -0300

    Update Basic.ino

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -5,12 +5,12 @@
 
 void setup() {
   Wire.begin();           //Inicia a Interface I2C
-  Breeze.reset();         //Zera volume e níveis (Breeze não zera níveis e volume quando energizado)
+  Breeze.reset();         //Zera volume e níveis das bandas (Breeze não zera níveis e volume quando energizado)
 }
 
 
 void loop() {
-  Breeze.equalize(1,7);   //Exemplo de reforço do canal 1 (32Hz) para nível 7 [+6dB] 
-  Breeze.equalize(6,-7);  //Exemplo de atenuação do canal 6 (1KHz) para nível -7 [-6dB]
-  Breeze.volume (-2);     //Exemplo de atenuação do volume para nível -2 (Breeze permite apenas atenuação de volume 0 a -47)
+  Breeze.equalize(1,7);   //Exemplo de reforço da banda 1 (32Hz) para nível 7 [+6dB] 
+  Breeze.equalize(6,-7);  //Exemplo de atenuação da banda 6 (1KHz) para nível -7 [-6dB]
+  Breeze.volume (-2);     //Exemplo de atenuação do volume para nível -2 (Breeze permite atenuação de volume 0 a -47)
 }
